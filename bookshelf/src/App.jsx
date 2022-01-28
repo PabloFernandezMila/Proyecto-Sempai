575eb593e18076aab886287566b8478805b44deb
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-01-28 09:23:48 -0300

    Changed app.js to jsx, added icon, changed title

--- bookshelf/src/App.jsx
+++ bookshelf/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to teesssst.
+          Edit <code>src/App.js</code> and save to aaaa.
         </p>
         <a
           className="App-link"
