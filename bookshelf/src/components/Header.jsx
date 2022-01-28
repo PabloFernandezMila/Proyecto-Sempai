bf6863154dd3e31ee0714696ba213a702cb3ac3a
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-01-28 10:54:03 -0300

    Moved images, created header component

---
+++ bookshelf/src/components/Header.jsx
@@ -0,0 +1,41 @@
+import "../assets/styles/headerAndFooter.css"
+
+export function Header() {
+    return (    <header>
+        <div className="burger-wrapper">
+            <div id="burger-menu" className="js-burger-menu">
+                <div id="burger-menu-icon"></div>
+            </div>
+        </div>
+        <div id="logo-wrapper">
+            <a target="_self" id="logo" href="index.html" className="roboto-white">Bookshelf</a>
+        </div>
+        <nav>
+            <ul>
+                <li className="nav_bar-elements active">
+                    <a target="_self" href="index.html" className="roboto-white">Home</a>
+                </li>
+                <li className="nav_bar-elements inactive">
+                    <a target="_self" href="catalog.html" className="roboto-white">Catalog</a>
+                </li>
+                <li className="nav_bar-elements inactive">
+                    <a target="_self" href="login.html" className="roboto-white">Login</a>
+                </li>
+                <li className="nav_bar-elements inactive">
+                    <a target="_self" href="about.html" className="roboto-white">About</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div id="search-login">
+            <form className="search js-search-form" autoComplete="off">
+                <div className="search-button js-search-button" tabIndex="0">
+                    <div className="search-button-image"></div>
+                </div>
+                <div className="search-field roboto-white">
+                    <input id="search-text" type="text" placeholder="Title, author or genres" className="roboto-white js-search-input" maxLength="35" /></div>
+            </form>
+            </div>
+    </header>);
+}
+
