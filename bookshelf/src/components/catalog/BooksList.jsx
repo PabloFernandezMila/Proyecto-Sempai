efc6b0563b0182f2ea1e9fdec65b7572b358b9b7
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-02-05 21:02:02 -0300

    Added card props

--- bookshelf/src/components/catalog/BooksList.jsx
+++ bookshelf/src/components/catalog/BooksList.jsx
@@ -1,8 +1,35 @@
 import { BookCard } from "./BookCard";
-export function BooksList() {
-  return (
-    <section className="catalog-section">
-      <BookCard></BookCard>
-    </section>
-  );
+import { api } from "../../api/api.js";
+import { useEffect } from "react";
+import { useState } from "react";
+
+export function BooksList(props) {
+  const [booksList, setBookList] = useState([]);
+
+  //Get info from Json Server
+  useEffect(() => {
+    //Get books
+    api.get("/books").then(function (response) {
+      const books = response.data;
+
+      //Update list with the books retrieved from the server
+      setBookList(books);
+    });
+  }, []);
+
+  //For each book retrieve from the server, the system maps the book properties into a Book card
+  const booksFromDB = booksList.map(function (book) {
+    return (
+      <BookCard
+        key={book.id}
+        bookTitle={book.bookTitle}
+        bookBackgroundImageURL={book.bookBackgroundImageURL}
+        bookDescription={book.bookDescription}
+        bookAuthor={book.bookAuthor}
+      />
+    );
+  });
+
+  //Return a collection of book cards
+  return <section className="catalog-section">{booksFromDB}</section>;
 }
