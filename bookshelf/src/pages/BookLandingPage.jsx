22992dbdd98dce0049eefaa4c0f4200955547347
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-02-10 02:06:26 -0300

    Fixed warning when redirected to Catalog from Booklanding page

--- bookshelf/src/pages/BookLandingPage.jsx
+++ bookshelf/src/pages/BookLandingPage.jsx
@@ -10,6 +10,7 @@ import { api } from "../api/api";
 import { useEffect } from "react";
 import { useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import { UnderConstructionPage } from "./UnderConstructionPage";
 
 // Breadcrumb variables
 const firstLinkTo = "/home";
@@ -23,6 +24,7 @@ export function BookLandingPage(props) {
   let id = params.id;
   //Remove the : character
   let idOnly = id.replace(":", "");
+  const [bookFound, setBookFound] = useState(false);
 
   const [bookInformation, setBookInformation] = useState([]);
   //Get info from Json Server
@@ -31,65 +33,72 @@ export function BookLandingPage(props) {
     const bookURL = "http://localhost:4000/books?id=" + idOnly;
     api.get(bookURL).then(function (response) {
       const book = response.data;
+      setBookFound(book.length > 0);
 
       //Update list with the book first book returned
       setBookInformation(book[0]);
     });
   }, [idOnly]);
 
-  // Set of book variables
-  const bookBackgroundImageURL = bookInformation.bookBackgroundImageURL;
-  const bookTitle = bookInformation.bookTitle;
-  const bookFullDescription = "" + bookInformation.bookFullDescription;
-  const bookAuthor = bookInformation.bookAuthor;
-  const bookCategory = bookInformation.bookCategory;
+  if (bookFound) {
+    // Set of book variables
+    const bookBackgroundImageURL = bookInformation.bookBackgroundImageURL;
+    const bookTitle = bookInformation.bookTitle;
+    const bookFullDescription = "" + bookInformation.bookFullDescription;
+    const bookAuthor = bookInformation.bookAuthor;
+    const bookCategory = bookInformation.bookCategory;
 
-  return (
-    <div className="landingWrapper">
-      <Title title={bookTitle} subtitle={"By " + bookAuthor}></Title>
-      <Breadcrumb
-        firstLinkTo={firstLinkTo}
-        firstLinkLabel={firstLinkLabel}
-        secondLinkTo={secondLinkTo}
-        secondLinkLabel={secondLinkLabel}
-      ></Breadcrumb>
-      <section className="book-section">
-        <BookDetails
-          bookBackgroundImageURL={bookBackgroundImageURL}
-          bookFullDescription={bookFullDescription}
-          bookAuthor={bookAuthor}
-          bookCategory={bookCategory}
-        ></BookDetails>
-        <div className="tags">
-          <div>
-            <span className="label roboto-white">Author:</span>
-            <span>
-              <Link to={"/underConstruction"} className="roboto-white tag">
-                {bookAuthor}
-              </Link>
-            </span>
+    return (
+      <div className="landingWrapper">
+        <Title title={bookTitle} subtitle={"By " + bookAuthor}></Title>
+        <Breadcrumb
+          firstLinkTo={firstLinkTo}
+          firstLinkLabel={firstLinkLabel}
+          secondLinkTo={secondLinkTo}
+          secondLinkLabel={secondLinkLabel}
+        ></Breadcrumb>
+        <section className="book-section">
+          <BookDetails
+            bookBackgroundImageURL={bookBackgroundImageURL}
+            bookFullDescription={bookFullDescription}
+            bookAuthor={bookAuthor}
+            bookCategory={bookCategory}
+          ></BookDetails>
+          <div className="tags">
+            <div>
+              <span className="label roboto-white">Author:</span>
+              <span>
+                <Link to={"/underConstruction"} className="roboto-white tag">
+                  {bookAuthor}
+                </Link>
+              </span>
+            </div>
+            <div>
+              <span className="label roboto-white">Genre: </span>
+              <span>
+                <Link
+                  to={"/catalog"}
+                  className="roboto-white tag"
+                  //Added useEffect in order to prevent an error while passing the value to the APP component while rendering other component
+                  onClick={() =>
+                    props.setSelectedFilter(
+                      "/books?bookCategory=" + bookCategory
+                    )
+                  }
+                >
+                  {bookCategory}
+                </Link>
+              </span>
+            </div>
           </div>
-          <div>
-            <span className="label roboto-white">Genre: </span>
-            <span>
-              <Link
-                to={"/catalog"}
-                className="roboto-white tag"
-                //Added useEffect in order to prevent an error while passing the value to the APP component while rendering other component
-                onClick={props.setSelectedFilter(
-                  "/books?bookCategory=" + bookCategory
-                )}
-              >
-                {bookCategory}
-              </Link>
-            </span>
-          </div>
-        </div>
-        <RelatedContent
-          bookCategory={bookCategory}
-          idOnly={idOnly}
-        ></RelatedContent>
-      </section>
-    </div>
-  );
+          <RelatedContent
+            bookCategory={bookCategory}
+            idOnly={idOnly}
+          ></RelatedContent>
+        </section>
+      </div>
+    );
+  } else {
+    return <UnderConstructionPage></UnderConstructionPage>;
+  }
 }
