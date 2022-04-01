164d6e036f8bba31980f02dcbecd0ee040a82311
Pablo Fernandez <PabloFernandezMila@gmail.com>
2022-04-01 19:23:54 -0300

    Added error logic for greeting

--- src/components/commons/Greeting.jsx
+++ src/components/commons/Greeting.jsx
@@ -6,16 +6,24 @@ export function Greeting(props) {
   const [userNamDB, setUserName] = useState("");
 
   //This variable emulates having an logged in user ID
-  const loggedUserID = 1;
+  const loggedUserID = 11;
 
   useEffect(() => {
     //Get user info using the id to query the DB
     const userIDURL = "http://localhost:4000/users/" + loggedUserID + "/name";
-    api.get(userIDURL).then(function (response) {
-      //Set the name
-      setUserName(response.data);
-    });
+    api
+      .get(userIDURL)
+      .then(function (response) {
+        //Set the name
+        if (response.status === 200) {
+          setUserName(response.data.userName);
+        }
+      })
+      .catch((error) => {
+        setUserName("Guest");
+      });
   }, []);
+
   return (
     <div
       id="greeting-user"
